0381c56809506e2e2d950f773b81fd89fda49cee
Rashedul Alam <rashed464@outlook.com>
2025-09-29 16:20:06 +0600

    models added

--- obj/Debug/net9.0/LibraryPortalBlazorWebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/LibraryPortalBlazorWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryPortalBlazorWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2ebd4e66beb79c6b6c52d6ad6a265ceba257054")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0217e9a07550cc3a73c2e672078a640bf0b5a33f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryPortalBlazorWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryPortalBlazorWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
