12a809561d87d5874501815c2f3084dee6f76883
Rashedul Alam <rashed464@outlook.com>
2025-09-30 22:16:31 +0600

    git ignore file added

--- obj/Debug/net9.0/LibraryPortalBlazorWebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/LibraryPortalBlazorWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryPortalBlazorWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aea5a7dbd404b6d3346fe4c06428d042914d909a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd368267278a56520cfa047b5bd8abea293e6d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryPortalBlazorWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryPortalBlazorWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
